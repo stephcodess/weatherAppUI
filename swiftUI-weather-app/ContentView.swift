0a1a6974444fe7826fa29891b565775617e74dfb
stephcodess <stephcodess>
2023-05-19 05:51:52 +0100

    Initial Commit

---
+++ swiftUI-weather-app/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  swiftUI-weather-app
+//
+//  Created by MacBook Pro on 19/05/R5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftUI-weather-app/swiftUI_weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_weather_appApp.swift
+//  swiftUI-weather-app
+//
+//  Created by MacBook Pro on 19/05/R5.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
